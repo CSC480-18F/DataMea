8d5972ced4544fcbc0e7c9f984ff3231acc42c1e
andyvadnais <avadnais@oswego.edu>
2018-10-10 20:23:51 -0400

    added toString and sentimentPct

--- src/main/java/Engine/Email.java
+++ src/main/java/Engine/Email.java
@@ -10,13 +10,12 @@ import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.util.CoreMap;
 
 import javax.mail.*;
+import javax.mail.internet.ContentType;
 import javax.mail.internet.MimeMultipart;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 class Email {
 
@@ -24,11 +23,18 @@ class Email {
     double NEGTHRESH = .7;
     double NEUTHRESH = .5;
     double POSTHRESH = .3;
+    boolean runSentiment = false;
+    boolean extracted = false;
+    ArrayList<Email> emailsExtracted;
     ArrayList<String> sentences;
-    Message message;
     int[] sentimentScores = new int[5];
-    int overallSentiment;
+    double sentimentPct = 0.0;
+    String sentimentPctStr;
+    int overallSentiment, sentencesAnalyzed;
     String content, title;
+    MimeMultipart mp;
+    BodyPart bp;
+    Message message;
     Date date;
     Sender sender;
     Flags flags;
@@ -39,23 +45,61 @@ class Email {
     int VPOS = 4;
     int VMULT = 3;
 
-    public Email(Message m, Sender s, Boolean runSentiment) {
+    //TODO figure out how to extract title, date
+    public Email(MimeMultipart m, Sender s, Flags f){
+        emailsExtracted = new ArrayList<>();
+        extracted = true;
+        mp = m;
+        sender = s;
+        flags = f;
+        sentences = getSentences(content);
+    }
 
+    public Email(BodyPart b, Sender s, String t, Date d, Flags f){
+        emailsExtracted = new ArrayList<>();
+        extracted = true;
+        bp = b;
+        title = t;
+        date = d;
+        sender = s;
+        flags = f;
+        sentences = getSentences(content);
+    }
+
+    public Email(String c, Sender s, String t, Date d, Flags f){
+        emailsExtracted = new ArrayList<>();
+        extracted = true;
+        content = c;
+        sender = s;
+        title = t;
+        date = d;
+        flags = f;
+        sentences = getSentences(content);
+    }
+
+
+
+    public Email(Message m, Sender s, Boolean rs) {
+        emailsExtracted = new ArrayList<>();
         message = m;
+        runSentiment = rs;
 
         try {
-            sentences = getSentences(m);
+            //System.out.println("Content: \n" + m.getContent().toString());
+            content = getTextFromMessage(m);
+            if(content != null) sentences = getSentences(content);
             title = m.getSubject();
-            sender = new Sender(s.toString());
+            sender = s;
             date = m.getSentDate();
             flags = m.getFlags();
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (MessagingException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
+        initializeSentiment();
+    }
 
+    private void initializeSentiment() {
         int sentenceScore;
         double probability;
         Sentiment sentenceSentiment;
@@ -65,55 +109,56 @@ class Email {
             if (sentences != null) {
                 for (String sentence : sentences) {
                     if (sentence.endsWith(".") || sentence.endsWith("!") || sentence.endsWith("?")) {
-                        System.out.println("\n" + sentence);
+                        //System.out.println("\n" + sentence);
+                        sentencesAnalyzed++;
                         sentenceSentiment = analyzeSentiment(sentence);
                         sentenceScore = sentenceSentiment.score;
                         probability = sentenceSentiment.probability;
                         switch (sentenceScore) {
                             case 0:
-                                System.out.println(probability + " chance it is Very Negative");
+                                //System.out.println(probability + " chance it is Very Negative");
                                 if (probability > VNEGTHRESH) {
                                     this.sentimentScores[VNEG]++;
-                                    System.out.println("Incrementing Very Negative");
+                                    //System.out.println("Incrementing Very Negative");
                                 } else {
                                     this.sentimentScores[NEG]++;
-                                    System.out.println("Incrementing Negative");
+                                    //System.out.println("Incrementing Negative");
                                 }
                                 break;
                             case 1:
-                                System.out.println(probability + " chance it is Negative");
+                                //System.out.println(probability + " chance it is Negative");
                                 if (probability > NEGTHRESH) {
                                     this.sentimentScores[NEG]++;
-                                    System.out.println("Incrementing Negative");
+                                    //System.out.println("Incrementing Negative");
                                 } else {
                                     this.sentimentScores[NEU]++;
-                                    System.out.println("Incrementing Neutral");
+                                    //System.out.println("Incrementing Neutral");
                                 }
                                 break;
                             case 2:
-                                System.out.println(probability + " chance it is Neutral");
+                                //System.out.println(probability + " chance it is Neutral");
                                 if (probability > NEUTHRESH) {
                                     this.sentimentScores[NEU]++;
-                                    System.out.println("Incrementing Neutral");
+                                    //System.out.println("Incrementing Neutral");
                                 } else {
                                     this.sentimentScores[POS]++;
-                                    System.out.println("Incrementing Positive");
+                                    //System.out.println("Incrementing Positive");
                                 }
                                 break;
                             case 3:
-                                System.out.println(probability + " chance it is Positive");
+                                //System.out.println(probability + " chance it is Positive");
                                 if (probability > POSTHRESH) {
                                     this.sentimentScores[POS]++;
-                                    System.out.println("Incrementing Positive");
+                                    //System.out.println("Incrementing Positive");
                                 } else {
                                     this.sentimentScores[VPOS]++;
-                                    System.out.println("Incrementing Very Positive");
+                                    //System.out.println("Incrementing Very Positive");
                                 }
                                 break;
                             case 4:
-                                System.out.println(probability + " chance it is Very Positive");
+                                //System.out.println(probability + " chance it is Very Positive");
                                 this.sentimentScores[4]++;
-                                System.out.println("Incrementing Very Positive");
+                                //System.out.println("Incrementing Very Positive");
                                 break;
                             default:
                                 break;
@@ -124,6 +169,11 @@ class Email {
 
             overallSentiment = sentimentScores[VPOS] * VMULT + sentimentScores[POS] -
                     sentimentScores[NEG] - sentimentScores[VNEG] * VMULT;
+
+            if(sentencesAnalyzed > 0) sentimentPct = ((double)overallSentiment / sentencesAnalyzed) * 100;
+            else sentimentPct = 0;
+            DecimalFormat df = new DecimalFormat("0.##");
+            sentimentPctStr = df.format(sentimentPct) + "%";
         }
     }
 
@@ -148,23 +198,53 @@ it appears to be whenever there is a thread of replies
 
  */
 
-
-    private ArrayList<String> getSentences(Message message) throws MessagingException, IOException {
-        //System.out.println("Getting text from message");
+    private String getTextFromMessage(Message message) throws IOException, MessagingException {
         String result = "";
         if (message.isMimeType("text/plain")) {
-            //System.out.println("Message is plain text");
-            result = message.getContent().toString();
-            return _getSentences(result);
-        } else if (message.isMimeType("multipart/*")) {
-            //System.out.println("Message is multipart");
-            MimeMultipart mimeMultipart = (MimeMultipart) message.getContent();
-            return getTextFromMimeMultipart(mimeMultipart, false);
+            return message.getContent().toString();
         }
         return null;
     }
 
-    private ArrayList<String> _getSentences(String result) {
+
+
+/*    private String getTextFromMimeMultipart(
+            MimeMultipart mimeMultipart) throws IOException, MessagingException {
+
+        int count = mimeMultipart.getCount();
+        if (count == 0)
+            throw new MessagingException("Multipart with no body parts not supported.");
+        boolean multipartAlt = new ContentType(mimeMultipart.getContentType()).match("multipart/alternative");
+        if (multipartAlt)
+            // alternatives appear in an order of increasing
+            // faithfulness to the original content. Customize as req'd.
+            return getTextFromBodyPart(mimeMultipart.getBodyPart(count - 1));
+        String result = "";
+        for (int i = 0; i < count; i++) {
+            BodyPart bodyPart = mimeMultipart.getBodyPart(i);
+            String bodyText = getTextFromBodyPart(bodyPart);
+            if(bodyText != "") emailsExtracted.add(new Email(bodyText, sender, title, date, flags));
+            result += getTextFromBodyPart(bodyPart);
+        }
+        return result;
+    }
+
+    private String getTextFromBodyPart(
+            BodyPart bodyPart) throws IOException, MessagingException {
+
+        String result = "";
+        if (bodyPart.isMimeType("text/plain")) {
+            result = (String) bodyPart.getContent();
+        } else if (bodyPart.isMimeType("text/html")) {
+            String html = (String) bodyPart.getContent();
+            result = org.jsoup.Jsoup.parse(html).text();
+        } else if (bodyPart.getContent() instanceof MimeMultipart){
+            result = getTextFromMimeMultipart((MimeMultipart)bodyPart.getContent());
+        }
+        return result;
+    }*/
+
+    private ArrayList<String> getSentences(String result) {
         result = filter(result);
         //System.out.println("After filter:\n" + result);
         ArrayList<String> sentences = new ArrayList<String>();
@@ -182,37 +262,6 @@ it appears to be whenever there is a thread of replies
         return sentences;
     }
 
-
-    public ArrayList<String> getTextFromMimeMultipart(
-            MimeMultipart mimeMultipart, Boolean isRecursing) throws MessagingException, IOException {
-        String result = "";
-        int count = mimeMultipart.getCount();
-        for (int i = 0; i < count; i++) {
-            //System.out.println("Body Part: " + (i + 1));
-            BodyPart bodyPart = mimeMultipart.getBodyPart(i);
-            //System.out.println("bodyPart " + i + "\n" + bodyPart.getContent());
-            if (bodyPart.isMimeType("text/plain")) {
-                //System.out.println("Body part is plain text");
-                result = result + "\n" + bodyPart.getContent();
-                return _getSentences(result);
-            } else if (bodyPart.isMimeType("text/html")) {
-                //System.out.println("Body part is HTML");
-                String html = (String) bodyPart.getContent();
-                result = result + "\n" + org.jsoup.Jsoup.parse(html).text();
-                return _getSentences(result);
-            } else if (bodyPart.getContent() instanceof MimeMultipart) {
-                //System.out.println("Body part is another MimeMultipart object");
-                result = result + getTextFromMimeMultipart((MimeMultipart) bodyPart.getContent(), true).get(0);
-            }
-
-
-        }
-        if (isRecursing) {
-            ArrayList<String> temp = new ArrayList<String>();
-            temp.add(result);
-            return temp;
-        } else return _getSentences(result);
-    }
 
 
     static Sentiment analyzeSentiment(String message) {
@@ -257,7 +306,7 @@ it appears to be whenever there is a thread of replies
                 "^(J|j)an\\.", "^(F|f)eb\\.", "^(M|m)ar\\.", "^(A|a)pr\\.", "^(J|j)un\\.",
                 "^(A|a)ug\\.", "^(S|s)ep\\.", "^(O|o)ct\\.", "^(N|n)ov\\.", "^(D|d)ec\\."},
                 {"Dr", "Mr", "Mrs", "Professor", "January", "Februrary", "March", "April", "June", "August",
-                "September", "October", "November", "December"}};
+                        "September", "October", "November", "December"}};
 
         for (int i = 0; i < ABBR; i++) {
             newText = newText.replaceAll(abbreviations[0][i], abbreviations[1][i]);
@@ -285,9 +334,16 @@ it appears to be whenever there is a thread of replies
 
 
     public String toString() {
-        return "From: " + this.sender + "\nTitle:" + this.title + "\nDate: " + date + "\nFlags: " + flags.toString()
+        if(this.sentimentPctStr != null)
+            return "From: " + this.sender + "\nTitle:" + this.title + "\nDate: " + date + "\nFlags: " + flags.toString()
+                    + "\nSentiment: " + this.sentimentPctStr + "\n" + content;
+        else
+            return "From: " + this.sender + "\nTitle:" + this.title + "\nDate: " + date + "\nFlags: " + flags.toString()
                 + "\n" + content;
     }
 
+    public void display() {
+        //System.out.println(this.toString());
+    }
 
 }
