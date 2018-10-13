c583c640065612e905310351aa7364586b10e111
Dylan Rizzo <rizzo.dylan@live.com>
2018-10-13 18:27:44 -0400

    Added email alert functionality

--- src/main/java/Controllers/DashboardLoading.java
+++ src/main/java/Controllers/DashboardLoading.java
@@ -10,7 +10,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.mail.Session;
 import java.net.URL;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
 public class DashboardLoading implements Initializable {
@@ -26,6 +31,10 @@ public class DashboardLoading implements Initializable {
     //------------------Declaring Variables------------------//
     private static Stage myStage;
     private static BooleanProperty stopVideo = new SimpleBooleanProperty(false);
+    //---------Declaring Variables for Email Sending---------//
+    private static String appEmail_Username = "datameaapp@gmail.com";
+    private static String appEmail_Password = "CSC480HCI521";
+    private static  String appEmail_Recipient;
 
     public static void setStopVideoToTrue(){
         stopVideo.setValue(true);
@@ -37,6 +46,13 @@ public class DashboardLoading implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        appEmail_Recipient = DashboardLogin.getEmail();
+        String from = appEmail_Username;
+        String pass = appEmail_Password;
+        String to = appEmail_Recipient;
+        String subject = "Java send mail example";
+        String body = "Welcome to JavaMail!";
+        sendFromGMail(from, pass, to, subject, body);
         tutorialVideo.getEngine().load(
                 "https://www.youtube.com/embed/J---aiyznGQ?autoplay=1"
         );
@@ -49,5 +65,44 @@ public class DashboardLoading implements Initializable {
                 }
             }
         });
+
+        emailNotify.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                sendFromGMail(appEmail_Username, appEmail_Password, appEmail_Recipient, "DataMea Ready", "Body Stuff");
+            }
+        });
+    }
+
+    private static void sendFromGMail(String from, String pass, String to, String subject, String body) {
+        Properties properties = System.getProperties();
+        String host = "smtp.getProperties()";
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.user", from);
+        properties.put("mail.smtp.password", pass);
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+
+        Session session = Session.getDefaultInstance(properties);
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setFrom(new InternetAddress(from));
+            InternetAddress toAddress = new InternetAddress(to);
+
+            message.setSubject(subject);
+            message.setText(body);
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, from, pass);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        }
+        catch (AddressException ae) {
+            ae.printStackTrace();
+        }
+        catch (MessagingException me) {
+            me.printStackTrace();
+        }
     }
 }
